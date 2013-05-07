a59743d52f673053d0a5d4adc6ea6ad0733dce61
TiagoFonseca <tiago.fonseca@optigest.net>
2013-05-07 11:25:30 +0100

    segundo local apenas

--- connVariaveis.php
+++ connVariaveis.php
@@ -1,5 +1,5 @@
 <?php 
-	// update local apenas
+	// 2º update local apenas
 	$connStringLocal = "host=localhost port=5432 dbname=testes user=postgres password=qwerty";
 	$conStringGisgeo = "host=192.168.1.114 port=5432 dbname=realidade user=back_real password=rb019283";	
 	
