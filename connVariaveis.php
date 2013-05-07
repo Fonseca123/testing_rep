be3c36285d1aaa4211fdde74f196457a1c35dab9
Tiago <tiago.fonseca@optigest.net>
2013-05-07 10:01:26 +0100

    first commit

---
+++ connVariaveis.php
@@ -0,0 +1,7 @@
+<?php 
+
+	$connStringLocal = "host=localhost port=5432 dbname=testes user=postgres password=qwerty";
+	$conStringGisgeo = "host=192.168.1.114 port=5432 dbname=realidade user=back_real password=rb019283";	
+	
+	return $connStringLocal;
+?>
